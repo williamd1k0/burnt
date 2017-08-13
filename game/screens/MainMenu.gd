7383d1353b5f991a58f9b317ce8d968770893d7a
William Tumeo <tumeowilliam@gmail.com>
2017-08-13 05:43:25 -0300

    Add difficulty buttons

--- game/screens/MainMenu.gd
+++ game/screens/MainMenu.gd
@@ -4,7 +4,8 @@ func _ready():
 	pass
 
 func _on_Play_pressed():
-	print('PLAY')
+	get_node("AnimationPlayer").play("diff")
+	get_node("ui/Menus/Difficulty/Buttons/Easy").grab_focus()
 
 func _on_Quit_pressed():
 	get_tree().quit()
