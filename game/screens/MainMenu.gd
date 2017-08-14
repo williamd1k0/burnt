4351511d66e07101ee98fbb74d5f54d138bf7609
William Tumeo <tumeowilliam@gmail.com>
2017-08-14 04:18:03 -0300

    Add a menu stack

--- game/screens/MainMenu.gd
+++ game/screens/MainMenu.gd
@@ -1,11 +1,24 @@
 extends Node
 
+var stack = []
+
 func _ready():
-	pass
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("pause") and stack.size() > 1:
+		rollback_stack()
+
+func rollback_stack():
+	stack.pop_front()
+	get_node("MenuStack").play(stack[0])
 
 func _on_Play_pressed():
-	get_node("AnimationPlayer").play("diff")
-	get_node("ui/Menus/Difficulty/Buttons/Easy").grab_focus()
+	get_node("MenuStack").play("diff")
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+func _on_MenuStack_animation_started( name ):
+	if stack.empty() or stack[0] != name:
+		stack.push_front(name)
